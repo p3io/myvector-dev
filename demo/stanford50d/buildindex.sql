96367ae75c4be0107c540ccd8ba6da1e23f2af52
shankariyer <shankariyer@p3io.in>
2024-04-08 17:08:59 +0530

    Added URL of Glove dataset

--- demo/stanford50d/buildindex.sql
+++ demo/stanford50d/buildindex.sql
@@ -1,4 +1,5 @@
 -- MyVector Plugin Demo. Create table and load words + vector corresponding to :-
+-- https://nlp.stanford.edu/projects/glove/
 -- Wikipedia 2014 + Gigaword 5 (6B tokens, 400K vocab, uncased, 50d, 100d, 200d, & 300d vectors, 822 MB download): glove.6B.zip
 -- This demo uses the 50 dimension word vectors from above dataset.
 
